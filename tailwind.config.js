cfff22f28ec542a254d19ceef47cf050adf9b28d
Izam Ghali <izamghali18@gmail.com>
2023-10-26 14:45:54 +0700

    added Inter font family from google font, extended colors, and added layer base applying font-serif to anykind of text

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      serif: ['Inter', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        lightGreen: '#4DF09C', // base color
+        green: '#64B089',
+        darkGreen: '#5C7066',
+        brown: '#9B6B65',
+        redBurn: '#F05E4D'
+      }
+    },
   },
   plugins: [],
 }
