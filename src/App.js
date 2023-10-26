1ff7b82d10da1db8b6629a6838596516d6b578de
Izam Ghali <izamghali18@gmail.com>
2023-10-26 12:16:21 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
+          className="App-link bg-white text-blue-800"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
       </header>
     </div>
