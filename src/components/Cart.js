6c3ab6256e062d94a8eb9f1e5c226b575f7b91ff
Izam Ghali <izamghali18@gmail.com>
2023-10-29 15:31:19 +0700

    created handlePurchase function to store cart array & totalPrice to localStorage in JSON format. Added p-3 to cart bg & adjusted its height on mobile screen

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,31 +1,31 @@
 import React, {useState} from 'react';
 import { List } from './List';
 
-export const Cart = (props) => {
-
-    const {
-        addedCart, addToCartBtnClicked
-    } = props;
-
+export const Cart = ({ addedCart, addToCartBtnClicked }) => {
     const [ mouseHover, setMouseHover ] = useState(false);
 
     let totalPrice = 0;
-
     for (let counter = 0; counter < addedCart.length; counter++) {
         const currentValue = addedCart[counter].price;
         totalPrice += currentValue;
     }
 
+    function handlePurchase() {
+        localStorage.setItem('cart', JSON.stringify(addedCart))
+        localStorage.setItem('totalPrice', totalPrice)
+        alert('Congratulations! Your order has been stored in localStorage!')
+    }
+
     return (
         <>
             <div className='
-                bg-amber-950 ( NOTE:  background temporarily white)
+                bg-amber-950
                 lg:w-96 2xl:w-[35rem] lg:h-screen
-                py-6 px-6
+                md:py-6 md:px-6 px-3 py-3
                 flex flex-col justify-between
                 
                 // mobile
-                w-full h-2/6 sm:h-2/5
+                w-full h-2/4 sm:h-3/5
                 lg:static fixed bottom-0 left-0
             ' id='cart'>
                 <h2 className='heading-text text-white'>Cart</h2>
@@ -35,11 +35,6 @@ export const Cart = (props) => {
                     h-full my-3 overflow-scroll
                     space-y-3 flex items-center
                 ' id='list-container'>
-                    {/* {addedCart.map(menu => {
-                        return <List
-                            title={menu.title}
-                        />
-                    })} */}
                     { addedCart.length > 0 && addToCartBtnClicked ? 
                     <List addedCart={addedCart} /> 
                     : <h1 className='font-normal text-xl text-white'>The shopping cart is still empty, click item to add to cart</h1> }
@@ -53,6 +48,7 @@ export const Cart = (props) => {
                     '
                     onMouseEnter={() => {setMouseHover(true)}}
                     onMouseLeave={() => {setMouseHover(false)}}
+                    onClick={handlePurchase}
                 id="total-btn">
                     <span className=''>{mouseHover ? 'PURCHASE' : 'TOTAL'}</span>
                     <span className='text-lg font-bold'>${totalPrice}</span>
