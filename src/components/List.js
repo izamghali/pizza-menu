0aec77193fed2838611cc760d9538b47677eaa87
Izam Ghali <izamghali18@gmail.com>
2023-10-29 15:32:14 +0700

    adjusted List component layout on different screen sizes, It is already reponsive

--- src/components/List.js
+++ src/components/List.js
@@ -1,40 +1,36 @@
 import React from 'react';
 
-export const List = (props) => {
-
-    const {
-        addedCart
-
-    } = props;
-
-    const testArr = [1, 2, 3, 4, 5]
+export const List = ({addedCart}) => {
 
     return(
         <>
         {addedCart.map(menu => {
-            return <div className='
-                 bg-white
-                p-4 rounded-md
+            return <div className='bg-white
+                p-4 rounded-md 
+                lg:block flex gap-2 sm:flex-row-reverse justify-between
+                lg:h-auto sm:h-[10rem] max-h-[20rem]
             '
                 onClick={() => {console.log(addedCart)}}
             >
                 {/* img container */}
-                <div>
-                    <img/>
+                <div className='lg:w-full  min-w-[50%] lg:h-[5rem] h-auto sm:block hidden'>
+                    <img className='w-full h-full object-cover lg:rounded-tl-md lg:rounded-br-none rounded-tr-md rounded-br-md' src={menu.img}/>
                 </div>
 
                 {/* content container */}
-                <div className='flex lg:flex-col flex-row justify-between'>
-                    <div>
-                        <h4 className='font-semibold text-lg'>{menu.title}</h4>
-                        {menu.topings.map(toping => {
-                            return <span className='text-slate-600'>
-                                { toping }
-                                {menu.topings.length > 1 && menu.topings[menu.topings.length - 1] !== toping ? ', ' : ''}
-                            </span>})
-                        }
+                <div className='flex flex-col'>
+                    <h4 className='font-semibold text-lg'>{menu.title}</h4>
+                    <div className='lg:block'>
+                        <div className=''>
+                            {menu.topings.map(toping => {
+                                return <span className='text-slate-600'>
+                                    { toping }
+                                    {menu.topings.length > 1 && menu.topings[menu.topings.length - 1] !== toping ? ', ' : ''}
+                                </span>})
+                            }
+                        </div>
+                        <p className='font-semibold text-lg lg:text-right'>${menu.price}</p>
                     </div>
-                    <p className='font-semibold text-lg'>${menu.price}</p>
                 </div>
             </div>
         })}
